1c21dc74b2d0cd3b12c89335d1644fc608b72a7c
nemo <138976623+Nemocccc@users.noreply.github.com>
2024-08-04 00:07:04 +0800

    new

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Link from 'next/link';
-import styles from './button.module.css';
+import styles from '/@components/Button/Button.module.css';
 
 export default function Button({ text, url} : {text: string, url: string}) {
     return (
